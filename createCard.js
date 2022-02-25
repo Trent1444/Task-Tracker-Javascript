c2ef81b7f1ce1c3a92378a55e5b433995d4a6700
TrentDev <TrentLathim@gmail.com>
2022-02-25 10:09:02 -0800

    added input field for entering name of card

--- createCard.js
+++ createCard.js
@@ -1,5 +1,15 @@
-const cards = [];
+// creating user task cards container array
+let cards = [];
+
+// creating task cards for user
 const createCard = () => {
-  const cardContainer = document.querySelector("taskCardContainer");
-  
+  if (cards.length >= 20) {
+    return;
+  } else {
+    cards.length++;
+    const cardSelector = document.querySelector(".taskCardContainer");
+    const card = document.createElement("button");
+    cardSelector.append(card);
+    card.classList.add("taskCard");
+  }
 };
